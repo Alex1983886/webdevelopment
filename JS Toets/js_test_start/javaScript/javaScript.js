04531b0f07c13257f7d83761e98decdcf0430e7b
Alex1983886 <alex.smarandoiu@student.vives.be>
2024-03-20 19:07:27 +0100

    sus

--- JS Toets/js_test_start/javaScript/javaScript.js
+++ JS Toets/js_test_start/javaScript/javaScript.js
@@ -1,40 +1,54 @@
+let gekozenFoto = "";
+function toonFoto(keuze) {
+    gekozenFoto = keuze;
+    const foto = document.getElementById("img");
+    const zinOnderFoto = document.getElementById("note");
+    switch (keuze) {
+        case "kip_zonder_ei":
+            foto.classList.remove("hidden");
+            zinOnderFoto.innerHTML = "Hierboven, een kip zonder ei";
+            break;
+        case "kip_met_ei":
+            foto.classList.remove("hidden");
+            foto.classList.add("with-egg");
+            zinOnderFoto.innerHTML = "Hierboven, een kip met ei";
+            break;
+        default:
+            foto.classList.add("hidden");
+    }
+    refresh();
+}
 const setup = () => {
-    function toonFoto(keuze) {
-        var foto = document.getElementById("img");
-        switch (keuze) {
-            case "kip_zonder_ei":
-                foto.src = ""
-                break;
-            case "kip_met_ei":
-                foto.src = "";
-                break;
-            default:
-                foto.src = "";
-        }
-    }
-    if (select === kip_zonder_ei) {
-        zin = "Hierboven, een kip zonder ei";
-        console.log(zin);
-        let index = zin.indexOf("text_letter")
-        let teller = 0;
-        while (index > -1) {
-            index =  zin.indexOf("text_letter", index + 1)
-            teller++;
-        }
-        console.log(teller);
-    } else if (select === kip_met_ei) {
-        zin2 = "Hierboven, een kip met ei";
-        console.log(zin2);
-        let index = zin.indexOf("text_letter")
-        let teller = 0;
-        while (index > -1) {
-            index =  zin.indexOf("text_letter", index + 1)
-            teller++;
-        }
-        console.log(teller);
-    } else {
-        console.log("");
-    }
+
+    const zinVanFoto = document.getElementById("text_letter");
+    zinVanFoto.addEventListener("input", (event ) => {
+        refresh();
+    })
 
 }
+const refresh = () => {
+    const countOnderaan = document.getElementById("count");
+    if (gekozenFoto === "kip_zonder_ei") {
+        zin = "Hierboven, een kip zonder ei";
+        console.log(zin);
+        let index = zin.indexOf(event.data)
+        let teller = 0;
+        while (index > -1) {
+            index =  zin.indexOf(event.data, index + 1)
+            teller++;
+        }
+        countOnderaan.innerHTML = teller;
+    } else if (gekozenFoto === "kip_met_ei") {
+        zin = "Hierboven, een kip met ei";
+        console.log(zin);
+        let index = zin.indexOf(event.data)
+        let teller = 0;
+        while (index > -1) {
+            index =  zin.indexOf(event.data, index + 1)
+            teller++;
+        }
+        countOnderaan.innerHTML = teller;
+    }
+}
+
 window.addEventListener("load", setup);
