f89879372b2a42c239c7a99a822bf45734adcc0b
Alex1983886 <alex.smarandoiu@student.vives.be>
2024-03-28 19:35:59 +0100

    Labo 7

--- Labo 7 JS/BeginSituatie_HitAnObject/BeginSituatie_HitAnObject/scripts/code.js
+++ Labo 7 JS/BeginSituatie_HitAnObject/BeginSituatie_HitAnObject/scripts/code.js
@@ -8,10 +8,43 @@ let global = {
     timeoutId: 0 // id van de timeout timer, zodat we die kunnen annuleren
 };
 const setup = () => {
-
+    let image = document.getElementById("foto");
+    image.addEventListener("click", onImageClick);
+    consoleBericht();
 };
 
+const setImageInterval = () => {
+    clearInterval(global.timeoutId);
+    global.timeoutId = setInterval( () => {
+        randomizeImage();
+        randomizePosition();
+    }, global.MOVE_DELAY);
+}
 
+const onImageClick = () => {
+    randomizeImage();
+    randomizePosition();
+    setImageInterval();
+}
+
+const randomizePosition = () => {
+    let randomHorizontaal = Math.floor(Math.random() * 600 - global.IMAGE_SIZE);
+    let randomVerticaal = Math.floor(Math.random() * 800 - global.IMAGE_SIZE);
+    let image = document.getElementById("foto");
+    image.style.marginLeft = randomHorizontaal + "px";
+    image.style.marginTop = randomVerticaal + "px";
+}
+
+const randomizeImage = () => {
+    const randomGetal = Math.floor(Math.random() * global.IMAGE_COUNT);
+    let image = document.getElementById("foto");
+    image.setAttribute("src", global.IMAGE_PATH_PREFIX + randomGetal + global.IMAGE_PATH_SUFFIX);
+}
+const consoleBericht = () => {
+    let intervalConsole = setInterval(() => {
+        console.log("Berichtje")
+    },1000)
+}
 
 
 window.addEventListener("load", setup);
