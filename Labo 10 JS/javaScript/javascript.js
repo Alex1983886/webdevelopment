24c1c17219122aa72734e452a17003a4ba2e21d4
Alex1983886 <alex.smarandoiu@student.vives.be>
2024-05-01 18:59:16 +0200

    labo

--- Labo 10 JS/javaScript/javascript.js
+++ Labo 10 JS/javaScript/javascript.js
@@ -1,24 +1,35 @@
+let geschiedenis = [];
 const setup = () => {
-    let geschiedenis = [];
-    let invoer = document.getElementsById("zoekInput").value();
-
-    switch (invoer) {
-        case 'g':
-            url =
-                break;
-        case 'y':
-            url =
-                break;
-        case 't':
-            url =
-                break;
-        case 'i':
-            ulr =
-                break;
+}
+const clickSearch = () => {
+    let invoer = document.getElementById("zoekInput").value;
+    let bewerkteInvoer = invoer.slice(3);
+    bewerkteInvoer = bewerkteInvoer.replace(" ", "+");
+    switch (true) {
+        case invoer.startsWith("/g "):
+            window.open('http://www.google.com/search?q=' + bewerkteInvoer, '_blank')
+            break;
+        case invoer.startsWith("/y "):
+            window.open('http://www.youtube.com/results?search_query=' + bewerkteInvoer, '_blank')
+            break;
+        case invoer.startsWith("/t "):
+            window.open('https://twitter.com/hashtag/' + bewerkteInvoer, '_blank')
+            break;
+        case invoer.startsWith("/i "):
+            window.open('http://www.instagram.com/explore/tags/' + bewerkteInvoer, '_blank')
+            break;
         default:
-            alert('De prefix is niet gekend.');
+            if (invoer.startsWith("/")) {
+                alert('Unknown Command Prefix');
+            } else {
+                alert('Invalid command');
+            }
             return;
     }
-
 }
+
+
+const goButton = document.getElementById("buttonGo");
+goButton.addEventListener("click", clickSearch);
+
 window.addEventListener("load", setup);
