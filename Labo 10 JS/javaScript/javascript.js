db8d65e14f22b74c0ef355fa0c4904f7fce508a1
Alex1983886 <alex.smarandoiu@student.vives.be>
2024-05-01 17:59:12 +0200

    test

---
+++ Labo 10 JS/javaScript/javascript.js
@@ -0,0 +1,24 @@
+const setup = () => {
+    let geschiedenis = [];
+    let invoer = document.getElementsById("zoekInput").value();
+
+    switch (invoer) {
+        case 'g':
+            url =
+                break;
+        case 'y':
+            url =
+                break;
+        case 't':
+            url =
+                break;
+        case 'i':
+            ulr =
+                break;
+        default:
+            alert('De prefix is niet gekend.');
+            return;
+    }
+
+}
+window.addEventListener("load", setup);
