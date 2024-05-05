8ca2954e74ee2fbde0abf31bfea4a756eb3c22d2
Alex1983886 <alex.smarandoiu@student.vives.be>
2024-05-05 10:47:52 +0200

    labo 10 af

--- Labo 10 JS/javaScript/javascript.js
+++ Labo 10 JS/javaScript/javascript.js
@@ -1,35 +1,109 @@
-let geschiedenis = [];
-const setup = () => {
+let myGlobal = {
+    history: []
 }
-const clickSearch = () => {
-    let invoer = document.getElementById("zoekInput").value;
-    let bewerkteInvoer = invoer.slice(3);
-    bewerkteInvoer = bewerkteInvoer.replace(" ", "+");
-    switch (true) {
-        case invoer.startsWith("/g "):
-            window.open('http://www.google.com/search?q=' + bewerkteInvoer, '_blank')
-            break;
-        case invoer.startsWith("/y "):
-            window.open('http://www.youtube.com/results?search_query=' + bewerkteInvoer, '_blank')
-            break;
-        case invoer.startsWith("/t "):
-            window.open('https://twitter.com/hashtag/' + bewerkteInvoer, '_blank')
-            break;
-        case invoer.startsWith("/i "):
-            window.open('http://www.instagram.com/explore/tags/' + bewerkteInvoer, '_blank')
-            break;
-        default:
-            if (invoer.startsWith("/")) {
-                alert('Unknown Command Prefix');
-            } else {
-                alert('Invalid command');
-            }
-            return;
+
+const setupPage = () => {
+    document.getElementById('buttonGo').addEventListener('click', clickSearch);
+    if(localStorage.getItem('history') !== null) {
+        restoreHistory();
     }
 }
 
+const clickSearch = () => {
+    let command = document.getElementById('commandInput').value.trim();
+    if(command.charAt(0) !== '/' || command.charAt(2) !== ' ') {
+        alert('Invalid command');
+    }
+    else {
+        let prefix = command.charAt(1);
+        let searchQuery = command.slice(3);
+        let formattedSearchQuery = searchQuery.replace(/ /g, "+"); // Vervang spaties door plusjes voor URL
+        let url;
+        switch (prefix) {
+            case 'g':
+                url = 'https://www.google.com/search?q=' + formattedSearchQuery;
+                window.open(url);
+                updateHistory('Google', searchQuery, url);
+                break;
+            case 'y':
+                url = 'https://www.youtube.com/results?search_query=' + formattedSearchQuery;
+                window.open(url);
+                updateHistory('YouTube', searchQuery, url);
+                break;
+            case 't':
+                url = 'https://twitter.com/hashtag/' + formattedSearchQuery;
+                window.open(url);
+                updateHistory('Twitter', searchQuery, url);
+                break;
+            case 'i':
+                url = 'https://www.instagram.com/explore/tags/' + formattedSearchQuery;
+                window.open(url);
+                updateHistory('Instagram', searchQuery, url);
+                break;
+            default:
+                alert('Unknown command prefix');
+        }
+    }
+}
 
-const goButton = document.getElementById("buttonGo");
-goButton.addEventListener("click", clickSearch);
+const updateHistory = (platform, searchQuery, url) => {
+    myGlobal.history.push({
+        title: platform,
+        text: searchQuery,
+        url: url
+    });
+    createCard(platform, searchQuery, url);
+    localStorage.setItem('history', JSON.stringify(myGlobal.history));
+}
 
-window.addEventListener("load", setup);
+const createCard = (platform, searchQuery, url) => {
+    let col4 = createElementWithClassName("div", "col-4");
+    let card = createElementWithClassName("div", "card");
+    card.classList.add(platform.toLowerCase()+"-card");
+    let cardBody = createElementWithClassName("div", "card-body");
+    let cardTitle = createElementWithClassNameAndText("h5", "card-title", platform); // Alleen platform in de titel
+    let cardText = createElementWithClassNameAndText("p", "card-text", searchQuery);
+    let linkGo = createLinkButton(url);
+    linkGo.classList.add(platform.toLowerCase()+"-button");
+
+    cardBody.appendChild(cardTitle);
+    cardBody.appendChild(cardText);
+    cardBody.appendChild(linkGo);
+    card.appendChild(cardBody);
+    col4.appendChild(card);
+
+    let row = document.querySelector("#resultatenContainer > .row");
+    row.appendChild(col4);
+}
+
+const createElementWithClassName = (element, className) => {
+    let newElement = document.createElement(element);
+    newElement.setAttribute("class", className);
+    return newElement;
+}
+
+const createElementWithClassNameAndText = (element, className, text) => {
+    let newElement = createElementWithClassName(element, className);
+    newElement.appendChild(document.createTextNode(text));
+    return newElement;
+}
+
+const createLinkButton = (url) => {
+    let newButton = document.createElement("a");
+    newButton.href = url;
+    newButton.classList.add('btn');
+    newButton.classList.add('btn-primary');
+    newButton.target = '_blank';
+    newButton.appendChild(document.createTextNode("Go!"));
+    return newButton;
+}
+
+const restoreHistory = () => {
+    myGlobal.history = JSON.parse(localStorage.getItem('history'));
+
+    for(let i = 0; i < myGlobal.history.length; i ++) {
+        createCard(myGlobal.history[i].title, myGlobal.history[i].text, myGlobal.history[i].url);
+    }
+}
+
+window.addEventListener("load", setupPage);
